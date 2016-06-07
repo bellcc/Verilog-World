a8d97b3dc1a229cb20b9889b84d2abaa64d4ba0b
firephyz <kyle.burge7196@gmail.com>
2016-06-07 12:08:00 -0400

    Fixed GDX dependencies

--- core/bin/modules/FullAdder.v
+++
@@ -1,7 +0,0 @@
-module FullAdder(bit1, bit2, sum, cin, cout);
-	input bit1, bit2, cin;
-	output sum, cout;
-	
-	assign sum = (bit1 ^ bit2) ^ cin;
-	assign cout = (bit1 & bit2) | (bit1 & cin) | (bit2 & cin);
-endmodule
