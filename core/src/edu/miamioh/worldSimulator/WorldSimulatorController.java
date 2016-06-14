d8a376245319af9a3c7ffc8d6e34fbb99fd9ed72
Clark <bellcc@miamioh.edu>
2016-06-14 14:00:30 -0700

    world simulator framework

--- core/src/edu/miamioh/worldSimulator/WorldSimulatorController.java
+++ core/src/edu/miamioh/worldSimulator/WorldSimulatorController.java
@@ -7,6 +7,29 @@
 
 package edu.miamioh.worldSimulator;
 
+import edu.miamioh.Level.Level;
+
 public class WorldSimulatorController {
+	
+	private static WorldSimulatorController currentController;
+	
+	private Level currentLevel;
+	
+	public WorldSimulatorController() {
+		
+		currentController = this;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public Level getCurrentLevel() {
+		return currentLevel;
+	}
+	
+	public static WorldSimulatorController getController() {
+		return currentController;
+	}
 
 }
