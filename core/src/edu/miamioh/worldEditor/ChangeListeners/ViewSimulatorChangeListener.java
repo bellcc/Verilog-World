8aaf805f8ef171a75458c5b8f1e1726fd0e3de7a
Clark <bellcc@miamioh.edu>
2016-06-22 14:02:49 -0400

    all features integrated

--- core/src/edu/miamioh/worldEditor/ChangeListeners/ViewSimulatorChangeListener.java
+++ core/src/edu/miamioh/worldEditor/ChangeListeners/ViewSimulatorChangeListener.java
@@ -3,6 +3,8 @@ package edu.miamioh.worldEditor.ChangeListeners;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
+import edu.miamioh.Level.Level;
+import edu.miamioh.verilogWorld.VerilogWorldMain;
 import edu.miamioh.worldEditor.WorldEditorController;
 import edu.miamioh.worldSimulator.WorldSimulatorController;
 
@@ -12,10 +14,13 @@ public class ViewSimulatorChangeListener extends ChangeListener {
 	public void changed(ChangeEvent event, Actor actor) {
 
 		System.out.println("View Simulator Change Listener");
-//		
-//		WorldEditorController.getCurrentController().resetMultiplexer();
-//
-//		WorldSimulatorController.getController().setCurrentLevel(WorldEditorController.getCurrentController().getCurrentLevel());
+
+		WorldEditorController.getCurrentController().resetMultiplexer();
+		
+		Level currentLevel = WorldEditorController.getCurrentController().getCurrentLevel();
+		WorldSimulatorController.getController().setCurrentLevel(currentLevel);
+		
+		VerilogWorldMain.getVerilogWorldMain().setWorldSimulatorScreen();
 
 	}
 
