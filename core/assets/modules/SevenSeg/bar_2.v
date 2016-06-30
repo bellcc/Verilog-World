6a5eaf06431bed6a752229fe2f5dd88155bf5389
firephyz <kyle.burge7196@gmail.com>
2016-06-29 21:38:05 -0400

    Created verilog files for a seven segement display tester

---
+++ core/assets/modules/SevenSeg/bar_2.v
@@ -0,0 +1,14 @@
+module bar_2(color, data);
+	output [23:0]color;
+	input data;
+	
+	if (data == 0)
+	begin
+		assign color = (255 << 16) | (255 << 8) | 255;
+	end
+	else
+	begin
+		assign color = (100 << 16) | (100 << 8) | 100;
+	end
+	
+endmodule
