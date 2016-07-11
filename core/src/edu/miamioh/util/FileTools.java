dee9df24d840e6c4b41edb4f778dff1304eeaa1c
bdshaffer73 <ben@shafferhouse.org>
2016-07-11 16:46:35 -0400

    Finished clearing clearable warnings.

--- core/src/edu/miamioh/util/FileTools.java
+++ core/src/edu/miamioh/util/FileTools.java
@@ -5,22 +5,26 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class FileTools {
+    
+    public static void copyFile(File sourceFile, File destFile) {
 	
-	public static void copyFile(File sourceFile, File destFile) {
-		
-		try {
-		    if(!destFile.exists()) {
-		        destFile.createNewFile();
-		    }
-	
-		    FileInputStream source = new FileInputStream(sourceFile);
-		    FileOutputStream dest = new FileOutputStream(destFile);
-		    
-		    while(source.available() != 0) {
-		    	dest.write(source.read());
-		    }
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	try {
+	    if (!destFile.exists()) {
+		destFile.createNewFile();
+	    }
+	    
+	    FileInputStream source = new FileInputStream(sourceFile);
+	    FileOutputStream dest = new FileOutputStream(destFile);
+	    
+	    while (source.available() != 0) {
+		dest.write(source.read());
+	    }
+	    
+	    source.close();
+	    dest.close();
+	    
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
 }
