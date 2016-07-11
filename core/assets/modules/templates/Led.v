558ae0ce3ae844319b3efa8aa7080594228826ec
firephyz <kyle.burge7196@gmail.com>
2016-07-11 15:02:11 -0400

    Fixed Led template module

--- core/assets/modules/templates/Led.v
+++ core/assets/modules/templates/Led.v
@@ -1,14 +1,21 @@
 module Led(color, data);
 	output [23:0]color;
 	input data;
+
+	reg colorLine;
+
+	assign color = colorLine;
 	
-	if (data == 0)
+	always @(*)
 	begin
-		assign color = (80 << 16) | (80 << 8) | 80;
-	end
-	else
-	begin
-		assign color = (255 << 16) | (0 << 8) | 0;
+		if (data == 0)
+		begin
+			colorLine = (80 << 16) | (80 << 8) | 80;
+		end
+		else
+		begin
+			colorLine = (255 << 16) | (0 << 8) | 0;
+		end
 	end
 	
 endmodule
